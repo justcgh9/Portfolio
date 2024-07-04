cab184e80313f5c7d9e07bfb12c8195a87ca66ad
justcgh9 <a.kabardiyadi@innopolis.university>
2024-07-04 10:54:27 +0300

    Workflow linting stage

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/Portfolio', 
-    assetPrefix: '/Portfolio',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'imgs.xkcd.com',
-            port: '',
-            pathname: '/comics/**',
-          },
-        ],
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imgs.xkcd.com",
+        port: "",
+        pathname: "/comics/**",
       },
+    ],
+  },
 };
 
 export default nextConfig;
