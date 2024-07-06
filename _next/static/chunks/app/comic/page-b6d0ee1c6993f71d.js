19ce0b9929b0130081f7cf96c1feb1b59f21cdd3
justcgh9 <justcgh9@users.noreply.github.com>
2024-07-06 12:18:43 +0000

    deploy: bb6dd103592dac427357a775e48662056f025dd0

---
+++ _next/static/chunks/app/comic/page-b6d0ee1c6993f71d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[755],{5837:function(n,e,t){Promise.resolve().then(t.t.bind(t,8173,23)),Promise.resolve().then(t.t.bind(t,7410,23))},7410:function(){}},function(n){n.O(0,[376,173,971,23,744],function(){return n(n.s=5837)}),_N_E=n.O()}]);
