7a894525f855a721784f259030ee3bdf13fcc813
justcgh9 <a.kabardiyadi@innopolis.university>
2024-07-11 15:30:15 +0300

    Added jest support

--- portfolio/__tests__/dynamic.test.tsx
+++ portfolio/__tests__/dynamic.test.tsx
@@ -1,52 +1,51 @@
-import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
-import Home from '../src/app/page'
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Home from "../src/app/page";
 
-describe('Test dynamic content changes', () => {
-  test('shows the correct element on mouse enter', () => {
-    const {container} = render(<Home />);
+describe("Test dynamic content changes", () => {
+  test("shows the correct element on mouse enter", () => {
+    const { container } = render(<Home />);
 
-    const aboutElement = container.querySelector('#info-about')
-    const photosCarouselElement = container.querySelector('#info-photo')
-    const portfolioElement = container.querySelector('#info-portfolio')
-    const contactsElement = container.querySelector('#info-contact')
+    const aboutElement = container.querySelector("#info-about");
+    const photosCarouselElement = container.querySelector("#info-photo");
+    const portfolioElement = container.querySelector("#info-portfolio");
+    const contactsElement = container.querySelector("#info-contact");
 
-    expect(aboutElement).toHaveClass('info hide');
-    expect(photosCarouselElement).toHaveClass('info hide');
-    expect(portfolioElement).toHaveClass('info hide');
-    expect(contactsElement).toHaveClass('info hide');
+    expect(aboutElement).toHaveClass("info hide");
+    expect(photosCarouselElement).toHaveClass("info hide");
+    expect(portfolioElement).toHaveClass("info hide");
+    expect(contactsElement).toHaveClass("info hide");
 
-
-    const aboutLabel = screen.getByText('About me')
-    const photosLabel = screen.getByText('Photos')
-    const portfolioLabel = screen.getByText('Portfolio')
-    const contactsLabel = screen.getByText('Contact Me')
+    const aboutLabel = screen.getByText("About me");
+    const photosLabel = screen.getByText("Photos");
+    const portfolioLabel = screen.getByText("Portfolio");
+    const contactsLabel = screen.getByText("Contact Me");
     fireEvent.mouseEnter(aboutLabel);
 
-    expect(aboutElement).toHaveClass('info show');
-    expect(photosCarouselElement).toHaveClass('info hide');
-    expect(portfolioElement).toHaveClass('info hide');
-    expect(contactsElement).toHaveClass('info hide');
+    expect(aboutElement).toHaveClass("info show");
+    expect(photosCarouselElement).toHaveClass("info hide");
+    expect(portfolioElement).toHaveClass("info hide");
+    expect(contactsElement).toHaveClass("info hide");
 
     fireEvent.mouseEnter(photosLabel);
 
-    expect(aboutElement).toHaveClass('info hide');
-    expect(photosCarouselElement).toHaveClass('info show');
-    expect(portfolioElement).toHaveClass('info hide');
-    expect(contactsElement).toHaveClass('info hide');
+    expect(aboutElement).toHaveClass("info hide");
+    expect(photosCarouselElement).toHaveClass("info show");
+    expect(portfolioElement).toHaveClass("info hide");
+    expect(contactsElement).toHaveClass("info hide");
 
     fireEvent.mouseEnter(portfolioLabel);
 
-    expect(aboutElement).toHaveClass('info hide');
-    expect(photosCarouselElement).toHaveClass('info hide');
-    expect(portfolioElement).toHaveClass('info show');
-    expect(contactsElement).toHaveClass('info hide');
+    expect(aboutElement).toHaveClass("info hide");
+    expect(photosCarouselElement).toHaveClass("info hide");
+    expect(portfolioElement).toHaveClass("info show");
+    expect(contactsElement).toHaveClass("info hide");
 
     fireEvent.mouseEnter(contactsLabel);
 
-    expect(aboutElement).toHaveClass('info hide');
-    expect(photosCarouselElement).toHaveClass('info hide');
-    expect(portfolioElement).toHaveClass('info hide');
-    expect(contactsElement).toHaveClass('info show');
+    expect(aboutElement).toHaveClass("info hide");
+    expect(photosCarouselElement).toHaveClass("info hide");
+    expect(portfolioElement).toHaveClass("info hide");
+    expect(contactsElement).toHaveClass("info show");
   });
 });
--- portfolio/__tests__/photos.test.tsx
+++ portfolio/__tests__/photos.test.tsx
@@ -1,46 +1,44 @@
-import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
-import Home from '../src/app/page'
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Home from "../src/app/page";
 
-describe('Test photo carousel', () => {
-    test('shows the correct photo when clicking on photo', () => {
-        const {container} = render(<Home />);
-        const photosCarouselElement = container.querySelector('#info-photo');
-        const photosLabel = screen.getByText('Photos');
-        fireEvent.mouseEnter(photosLabel);
-        expect(photosCarouselElement).toHaveClass('info show');
+describe("Test photo carousel", () => {
+  test("shows the correct photo when clicking on photo", () => {
+    const { container } = render(<Home />);
+    const photosCarouselElement = container.querySelector("#info-photo");
+    const photosLabel = screen.getByText("Photos");
+    fireEvent.mouseEnter(photosLabel);
+    expect(photosCarouselElement).toHaveClass("info show");
 
-        const somePhoto = screen.getByAltText('Thumbnail 1');
-        fireEvent.click(somePhoto);
-        expect(screen.getByAltText('Photo 1')).toBeInTheDocument();
+    const somePhoto = screen.getByAltText("Thumbnail 1");
+    fireEvent.click(somePhoto);
+    expect(screen.getByAltText("Photo 1")).toBeInTheDocument();
+  });
 
+  test("Shows the correct photo when clicking on an arrow", () => {
+    const { container } = render(<Home />);
+    const photosCarouselElement = container.querySelector("#info-photo");
+    const photosLabel = screen.getByText("Photos");
+    fireEvent.mouseEnter(photosLabel);
+    expect(photosCarouselElement).toHaveClass("info show");
 
-    });
+    const nextArrow = container.querySelector("#next_btn");
+    const prevArrow = container.querySelector("#prev_btn");
 
-    test('Shows the correct photo when clicking on an arrow', () => {
-        const {container} = render(<Home />);
-        const photosCarouselElement = container.querySelector('#info-photo');
-        const photosLabel = screen.getByText('Photos');
-        fireEvent.mouseEnter(photosLabel);
-        expect(photosCarouselElement).toHaveClass('info show');
+    expect(prevArrow).toBeInTheDocument();
+    expect(nextArrow).toBeInTheDocument();
 
-        const nextArrow = container.querySelector('#next_btn');
-        const prevArrow = container.querySelector('#prev_btn');
+    const somePhoto = screen.getByAltText("Thumbnail 0");
+    fireEvent.click(somePhoto);
 
-        expect(prevArrow).toBeInTheDocument();
-        expect(nextArrow).toBeInTheDocument();
+    for (let i = 1; i <= 3; i++) {
+      fireEvent.click(nextArrow as Element);
+      expect(screen.getByAltText("Photo " + i)).toBeInTheDocument();
+    }
 
-        const somePhoto = screen.getByAltText('Thumbnail 0');
-        fireEvent.click(somePhoto);
-
-        for(let i = 1; i<= 3; i++){
-            fireEvent.click(nextArrow as Element);
-            expect(screen.getByAltText('Photo ' + i)).toBeInTheDocument();
-        }
-
-        for(let i = 2; i>= 0; i--){
-            fireEvent.click(prevArrow as Element);
-            expect(screen.getByAltText('Photo ' + i)).toBeInTheDocument();
-        }
-    });
+    for (let i = 2; i >= 0; i--) {
+      fireEvent.click(prevArrow as Element);
+      expect(screen.getByAltText("Photo " + i)).toBeInTheDocument();
+    }
+  });
 });
